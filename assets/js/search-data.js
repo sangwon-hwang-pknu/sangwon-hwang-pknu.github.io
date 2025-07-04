c7e1dd11430f88dc217cba5aca30ecb62ea7ab15
kihyuk-sung <kihyuk.sung@outlook.kr>
2025-07-04 08:08:52 +0000

    Deploying to gh-pages from @ sangwon-hwang-pknu/sangwon-hwang-pknu.github.io@6ad94499509ee2ebdca3500e09faa0d03c3d496d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,10 +32,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/https:/sangwon-hwang-pknu.github.io/assets/pdf/CV_SHwang.pdf";
           },
         },{id: "nav-teaching",
           title: "teaching",
