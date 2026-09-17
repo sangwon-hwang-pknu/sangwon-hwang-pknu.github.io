665c6d0d0d5d1f9553b9ba7fff0a142085c1c5d7
sangwon-hwang-pknu <s.won.hwang@pknu.ac.kr>
2026-09-17 13:54:51 +0000

    Deploying to gh-pages from @ sangwon-hwang-pknu/sangwon-hwang-pknu.github.io@6cfd8f2222907dd90a1220403c581eb2e709a309 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -151,13 +151,13 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_3.html";
-            },},{id: "news-aidan-lab-started-keti-research-project",
-          title: 'AIDAN Lab Started KETI Research Project',
+            },},{id: "news-aidan-lab-joined-keti-research-project",
+          title: 'AIDAN Lab Joined KETI Research Project',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_4.html";
-            },},{id: "news-aidan-lab-started-nrf-crg-project",
-          title: 'AIDAN Lab Started NRF CRG Project',
+            },},{id: "news-aidan-lab-joined-nrf-crg-project",
+          title: 'AIDAN Lab Joined NRF CRG Project',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_5.html";
